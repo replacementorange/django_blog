2fb5e0ee4f5fe3c143383d543f7a31681da90da5
replacementorange <162890042+replacementorange@users.noreply.github.com>
2025-04-09 23:15:46 +0300

    Added forms in detail.html

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -5,11 +5,11 @@ class CommentForm(forms.Form):
     author = forms.CharField(
         max_length=60,
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Write your name!"}
+            attrs={"class": "form-control", "placeholder": "Write your name here."}
         ),
     )
     body = forms.CharField(
         widget=forms.Textarea(
-            attrs={"class": "form-control", "placeholder": "Write your comment!"}
+            attrs={"class": "form-control", "placeholder": "Write your comment here."}
         )
     )
